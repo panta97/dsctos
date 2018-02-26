55c60af9816f280f6f0a03522118d27eb0f38e72
Roosevelt <panta_85.x@hotmail.com>
2018-02-26 16:14:03 -0500

    first approach

--- src/app/dscto/dscto.component.ts
+++ src/app/dscto/dscto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DecimalPipe, PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dscto',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DsctoComponent implements OnInit {
 
+  precioNormal:number;
+
+  tablas = [
+    {
+      porcentaje: 0.1,
+      aplicado: 0,
+      afectados: 'truzas medias, zapatillas'
+    },
+    {
+      porcentaje: 0.18,
+      aplicado: 0,
+      afectados: 'todas las ropas, pelotas mochilas'
+    },
+    {
+      porcentaje: 0.3,
+      aplicado: 0,
+      afectados: 'accesorios'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  calcularDsctos(precioNormal) {
+    this.tablas = this.tablas.map(ele => {
+      ele.aplicado = Math.round((1 - ele.porcentaje) * precioNormal);
+      return ele;
+    })
   }
 
 }
